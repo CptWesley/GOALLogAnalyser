168395dd80eeacc4013fa6cb212b9862993b910a
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-08-30 13:22:29 +0200

    Add query profile classes

---
+++ src/Analyzation/Queries/QueryProfile.cs
@@ -0,0 +1,144 @@
+﻿namespace GOALLogAnalyser.Analyzation.Queries
+{
+    /// <summary>
+    /// Class containing info regarding queries.
+    /// </summary>
+    public class QueryProfile
+    {
+        /// <summary>
+        /// Gets the amount of times this query succeeded.
+        /// </summary>
+        /// <value>
+        /// The amount of times this query succeeded.
+        /// </value>
+        public int Hits { get; private set; }
+        /// <summary>
+        /// Gets the amount of times this query failed.
+        /// </summary>
+        /// <value>
+        /// The amount of times this query failed.
+        /// </value>
+        public int Misses { get; private set; }
+        /// <summary>
+        /// Gets the amount of times this query was done.
+        /// </summary>
+        /// <value>
+        /// The amount of times this query was done.
+        /// </value>
+        public int Times => Hits + Misses;
+
+        /// <summary>
+        /// Gets the total time spent doing this query.
+        /// </summary>
+        /// <value>
+        /// The total time spent doing this query.
+        /// </value>
+        public long TotalTime { get; private set; }
+        /// <summary>
+        /// Gets the shortest time spent doing this query.
+        /// </summary>
+        /// <value>
+        /// The shortest time spent doing this query.
+        /// </value>
+        public long ShortestTime { get; private set; }
+        /// <summary>
+        /// Gets the longest time spent doing this query.
+        /// </summary>
+        /// <value>
+        /// The longest time spent doing this query.
+        /// </value>
+        public long LongestTime { get; private set; }
+        /// <summary>
+        /// Gets the average time spent doing this query.
+        /// </summary>
+        /// <value>
+        /// The average time spent doing this query.
+        /// </value>
+        public long AverageTime => GetAverageTime();
+
+        /// <summary>
+        /// Gets the query.
+        /// </summary>
+        /// <value>
+        /// The query.
+        /// </value>
+        public string Query { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QueryProfile"/> class.
+        /// </summary>
+        /// <param name="query">The query.</param>
+        public QueryProfile(string query)
+        {
+            Query = query;
+            Hits = 0;
+            Misses = 0;
+            TotalTime = 0;
+            ShortestTime = long.MaxValue;
+            LongestTime = long.MinValue;
+        }
+
+        private long GetAverageTime()
+        {
+            return TotalTime / (Hits + Misses);
+        }
+
+        /// <summary>
+        /// Adds the specified query data.
+        /// </summary>
+        /// <param name="time">The time.</param>
+        /// <param name="hit">if set to <c>true</c> [hit].</param>
+        public void Add(long time, bool hit)
+        {
+            if (hit)
+                ++Hits;
+            else
+                ++Misses;
+
+            TotalTime += time;
+            if (ShortestTime > time)
+                ShortestTime = time;
+            if (LongestTime < time)
+                LongestTime = time;
+        }
+
+        /// <summary>
+        /// Implements the operator +.
+        /// </summary>
+        /// <param name="a">First object.</param>
+        /// <param name="b">Second object.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static QueryProfile operator +(QueryProfile a, QueryProfile b)
+        {
+            QueryProfile res = new QueryProfile(a.Query)
+            {
+                Hits = a.Hits + b.Hits,
+                Misses = a.Misses + b.Misses,
+                TotalTime = a.TotalTime + b.TotalTime
+            };
+
+            if (a.ShortestTime < b.ShortestTime)
+            {
+                res.ShortestTime = a.ShortestTime;
+            }
+            else
+            {
+                res.ShortestTime = b.ShortestTime;
+            }
+
+            if (a.LongestTime > b.LongestTime)
+            {
+                res.LongestTime = a.LongestTime;
+            }
+            else
+            {
+                res.LongestTime = b.LongestTime;
+            }
+
+
+            return res;
+        }
+    }
+}
---
+++ src/Analyzation/Queries/QueryProfileCollection.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace GOALLogAnalyser.Analyzation.Queries
+{
+    /// <summary>
+    /// Class containing a collection of query profiles.
+    /// </summary>
+    /// <seealso cref="QueryProfileCollection" />
+    public class QueryProfileCollection : IEnumerable<QueryProfile>
+    {
+        private readonly List<QueryProfile> _profiles;
+
+        /// <summary>
+        /// Gets the count.
+        /// </summary>
+        /// <value>
+        /// The count.
+        /// </value>
+        public int Count => _profiles.Count;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QueryProfileCollection"/> class.
+        /// </summary>
+        public QueryProfileCollection()
+        {
+            _profiles = new List<QueryProfile>();
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="QueryProfile"/> with the specified i.
+        /// </summary>
+        /// <value>
+        /// The <see cref="QueryProfile"/>.
+        /// </value>
+        /// <param name="i">The i.</param>
+        /// <returns></returns>
+        public QueryProfile this[int i]
+        {
+            get => Get(i);
+            set => Set(i, value);
+        }
+
+        /// <summary>
+        /// Determines whether [contains] [the specified name].
+        /// </summary>
+        /// <param name="query">The query.</param>
+        /// <returns>
+        ///   <c>true</c> if [contains] [the specified name]; otherwise, <c>false</c>.
+        /// </returns>
+        public bool Contains(string query)
+        {
+            return IndexOf(query) != -1;
+        }
+
+        /// <summary>
+        /// Indexes the of given query.
+        /// </summary>
+        /// <param name="query">The query name.</param>
+        /// <returns>The index of the query. -1 if not found.</returns>
+        public int IndexOf(string query)
+        {
+            for (int i = 0; i < _profiles.Count; ++i)
+            {
+                if (_profiles[i].Query == query)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Adds the specified profile.
+        /// </summary>
+        /// <param name="profile">The profile.</param>
+        public void Add(QueryProfile profile)
+        {
+            _profiles.Add(profile);
+        }
+
+        /// <summary>
+        /// Removes the specified profile.
+        /// </summary>
+        /// <param name="profile">The profile.</param>
+        public void Remove(QueryProfile profile)
+        {
+            _profiles.Remove(profile);
+        }
+
+        /// <summary>
+        /// Gets the specified index.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        /// <returns></returns>
+        public QueryProfile Get(int index)
+        {
+            if (index >= _profiles.Count)
+                return null;
+            return _profiles[index];
+        }
+
+        /// <summary>
+        /// Sets the specified index.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        /// <param name="profile">The profile.</param>
+        public void Set(int index, QueryProfile profile)
+        {
+            if (index >= _profiles.Count)
+            {
+                _profiles.Add(profile);
+            }
+            else
+            {
+                _profiles[index] = profile;
+            }
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// An enumerator that can be used to iterate through the collection.
+        /// </returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public IEnumerator<QueryProfile> GetEnumerator()
+        {
+            return _profiles.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection.
+        /// </returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
